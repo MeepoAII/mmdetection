02d595a4fc82b6b564aa93a3d0338d27d98a67cf
MeepoAII <meepo.ai@gmail.com>
2020-12-04 10:23:18 +0800

    support crowdhuman dataset

--- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco-person.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco-person.py
@@ -1,6 +1,6 @@
 _base_ = './faster_rcnn_r50_fpn_1x_coco.py'
 model = dict(roi_head=dict(bbox_head=dict(num_classes=1)))
-classes = ('person', )
+classes = ('full', )
 data = dict(
     train=dict(classes=classes),
     val=dict(classes=classes),
